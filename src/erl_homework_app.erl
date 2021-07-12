06391bef02920d285938a2f2517abca0e2f1cac6
Denis Tankov <denis.tankov@eltex.loc>
2021-07-12 09:05:43 +0700

    rebar added

---
+++ src/erl_homework_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc erl_homework public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(erl_homework_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    erl_homework_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
